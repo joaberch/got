c45231b619382b1518aa8ddabcfe9f62d6a39d33
joaberch <122774888+joaberch@users.noreply.github.com>
2025-02-21 15:54:03 +0100

    refactor: use enum

--- MVP/main.go
+++ MVP/main.go
@@ -5,15 +5,35 @@ import (
 	"os"
 )
 
+type commandType int
+
+const (
+	CommandHello commandType = iota
+	Other
+)
+
+func getCommand(name string) (commandType, error) {
+	switch name {
+	case "hello":
+		return CommandHello, nil
+	default:
+		return -1, fmt.Errorf("Command unknown")
+	}
+}
+
 func main() {
 	if len(os.Args) < 2 {
 		fmt.Println("Usage: got hello")
 		return
 	}
 
-	if os.Args[1] == "hello" {
-		fmt.Println("got got got")
+	command, err := getCommand(os.Args[1])
+	if err != nil {
+		fmt.Println(err)
 		return
 	}
-	fmt.Println("Commande inconnue. Utilisez: got hello")
+
+	if command == CommandHello {
+		fmt.Println("got got got")
+	}
 }
