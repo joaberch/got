0529e3df6de3567695c42037aeaac9b85d943db3
joaberch <122774888+joaberch@users.noreply.github.com>
2025-09-10 16:33:14 +0200

    fix(init): split object from type

--- internal/model/files_list.go
+++ internal/model/files_list.go
@@ -1,7 +1,10 @@
 package model
 
 var FilesList = map[string]string{
-	"staging.csv": "File",   //Staging area
-	"commits.csv": "File",   //Tracking file state
-	"objects":     "Folder", //Contain objects
+	"staging.csv":     "File",   //Staging area
+	"commits.csv":     "File",   //Tracking file state
+	"objects":         "Folder", //Contain objects
+	"objects/commits": "Folder", //Contain serialized commits with hash as name
+	"objects/trees":   "Folder", //Contain serialized trees with hash as name
+	"objects/blobs":   "Folder", //Contain serialized blobs with hash as name
 }
