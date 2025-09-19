5799fd60cbe0f1d4b845cd25cf4d773f4b9d4c6f
joaberch <122774888+joaberch@users.noreply.github.com>
2025-09-19 16:28:28 +0200

    refactor(utils): return head content
    head is always only the hash of the latest commit so we can simply read all of the file and return it

--- utils/get_latest_commit_hash.go
+++ utils/get_latest_commit_hash.go
@@ -1,8 +1,6 @@
 package utils
 
 import (
-	"bufio"
-	"fmt"
 	"os"
 	"path/filepath"
 	"strings"
@@ -14,22 +12,9 @@ import (
 // If the file is empty, an empty string is returned.
 func GetLatestCommitHash() (string, error) {
 	headPath := filepath.Join(".got", "head")
-	file, err := os.Open(headPath)
+	data, err := os.ReadFile(headPath)
 	if err != nil {
-		return "", fmt.Errorf("failed to open head file at %s: %w", headPath, err)
+		return "", err
 	}
-	defer func() {
-		errClose := file.Close()
-		if errClose != nil {
-			err = fmt.Errorf("failed to close head file at %s: %w", headPath, errClose)
-		}
-	}()
-
-	var content = ""
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		line := scanner.Text()
-		content += line + ""
-	}
-	return strings.TrimSpace(content), nil
+	return strings.TrimSpace(string(data)), nil
 }
