42a47c1afa8504b22e0de75111ac5e0a8f8bd949
joaberch <122774888+joaberch@users.noreply.github.com>
2025-09-19 16:27:38 +0200

    refactor(utils): add blobDeserialize and
    Return simple struct instead of pointer

--- utils/deserialize_commit.go
+++ utils/deserialize_commit.go
@@ -8,8 +8,8 @@ import (
 // DeserializeCommit deserializes JSON-encoded data into a model.Commit and returns
 // a pointer to the resulting Commit along with any error produced by json.Unmarshal.
 // The function does no additional validation of the decoded value.
-func DeserializeCommit(data []byte) (*model.Commit, error) {
+func DeserializeCommit(data []byte) (model.Commit, error) {
 	var commit model.Commit
 	err := json.Unmarshal(data, &commit)
-	return &commit, err
+	return commit, err
 }
--- utils/deserialize_tree.go
+++ utils/deserialize_tree.go
@@ -8,8 +8,8 @@ import (
 // DeserializeTree unmarshals JSON-encoded data into a model.Tree and returns a pointer to it.
 // The input data must contain a JSON representation of a model.Tree. If unmarshalling fails,
 // the returned error describes the failure and the returned *model.Tree will be the zero-value instance.
-func DeserializeTree(data []byte) (*model.Tree, error) {
+func DeserializeTree(data []byte) (model.Tree, error) {
 	var tree model.Tree
 	err := json.Unmarshal(data, &tree)
-	return &tree, err
+	return tree, err
 }
