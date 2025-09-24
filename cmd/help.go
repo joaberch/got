0896b79ab2299e9c190245e9e0c390a02c9f7403
joaberch <122774888+joaberch@users.noreply.github.com>
2025-09-24 14:35:14 +0200

    fix(help): refactor help

--- cmd/help.go
+++ cmd/help.go
@@ -12,15 +12,15 @@ func ShowHelp() {
 		"  got <command> [arguments]\n" +
 		"\n" +
 		"Available Commands:\n" +
-		"  help, h         Show this help message\n" +
-		"  version, v      Display the current version of Got\n" +
-		"  init, i         Initialize a new Got repository\n" +
-		"  add, a <file>   Add a file to the staging area\n" +
-		"  status, s       Show the status of the working directory\n" +
-		"  commit, c <msg> Commit staged changes with a message\n" +
-		"  restore, r <id> Restore a file from a previous commit by hash\n" +
-		"  log, l          Display the log from the commits file\n" +
-		"  diff, d         Display the differences in the file from the last commit" + //TODO - get (distinct) all files from all commits and use all of them for a better diff display
+		"  help					Show this help message\n" +
+		"  version				Display the current version of Got\n" +
+		"  init					Initialize a new Got repository\n" +
+		"  add <file>			Add a file to the staging area\n" +
+		"  status				Show the status of the working directory\n" +
+		"  commit <msg>			Commit staged changes with a message\n" +
+		"  restore <id>			Restore a file from a previous commit by hash\n" +
+		"  log					Display the log from the commits file\n" +
+		"  diff					Display the differences in the file from the last commit" + //TODO - get (distinct) all files from all commits and use all of them for a better diff display
 		"\n" +
 		"Examples:\n" +
 		"  got init\n" +
